5df2f2aa4429c03d6d44a4146438d6c4735b66c4
nahom <nahomersom@gmail.com>
2022-03-22 16:41:27 +0300

    style:bottom navigation bar updated

---
+++ lib/Screens/mezmurs.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Mezmurs extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text('hi',style: TextStyle(
+         fontWeight: FontWeight.bold
+        ),),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/setting.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Setting extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Center(
+      child: Text('setting is loading'),
+    );
+  }
+}
