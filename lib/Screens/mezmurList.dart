57cc7620e158ce5699314197d24d86cd17b112d2
nahom <my.adresse@hotmail.com>
2022-03-22 21:33:22 +0300

    fix:buid.grdle

--- lib/Screens/mezmurList.dart
+++ lib/Screens/mezmurList.dart
@@ -1,10 +1,24 @@
-      import 'package:finote_birhan/Widgets/toogler.dart';
 import 'package:flutter/cupertino.dart';
-      import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart';
 import '../constants.dart';
-      class mezmurLists extends StatelessWidget{
-      List<Map> x = [
+class mezmurLists extends StatefulWidget{
+  @override
+  State<mezmurLists> createState() => _mezmurListsState();
+}
+
+class _mezmurListsState extends State<mezmurLists> with SingleTickerProviderStateMixin {
+
+  AnimationController? _controller;
+  Animation? _colorAnimation;
+  Animation<double>? _sizeAnimation;
+  @override
+  void initState(){
+
+  }
+  @override
+  void dispose(){
+  }
+  List<Map> x = [
       {'አይነት':'የአዲስ አመት','ምስል':'https://kjkjkjdkjk'},
       {'አይነት':'የመስቀል','ምስል':'https://kjkjkjdkjk'},
       {'አይነት':'የአቡነአረጋዊ','ምስል':'https://kjkjkjdkjk'},
@@ -18,6 +32,7 @@ import '../constants.dart';
             {'አይነት':'የአቡነአረጋዊ','ምስል':'https://kjkjkjdkjk'},
             {'አይነት':'የህዳር ሚካኤል','ምስል':'https://kjkjkjdkjk'}
       ];
+
       @override
       Widget build(BuildContext context) {
 
@@ -72,47 +87,10 @@ import '../constants.dart';
 
       ),
       ),
-      // flexibleSpace: FlexibleSpaceBar(
-      // title: Text('hdkjkdjk'),
-      // background: Text('kdjakjkjdkajjjjjjjjjjjjjjjjjjjjjj'),
-      // centerTitle: true,
-      // ),
-      // bottom: PreferredSize(child: Icon(Icons.linear_scale,size: 60.0,), preferredSize: Size.fromHeight(0)),
 
-      //     bottom: AppBar(
-      //     title: Container(
-      //       height: 45,
-      //       child: TextField(
-      //         decoration: InputDecoration(
-      //             border: OutlineInputBorder(),
-      //             hintText: 'Enter a search term'),
-      //       ),
-      //     ),
-      // // ),
-      // ),
       ),
       SliverList(delegate: SliverChildBuilderDelegate(
       (context, int index) {
-      // return ListTile(
-      // leading: Container(
-      // padding: EdgeInsets.all(6),
-      // width: 100,
-      // child: CircleAvatar(
-      //       radius: 550,
-      //       backgroundColor: Colors.brown.shade800,
-      //       child: const Text('AH'),
-      // )),
-      // title:
-      // Container(
-      // decoration: BoxDecoration(
-      //     border: Border(bottom: BorderSide(color: Colors.black26))),
-      // child: Text('Place ${x[index]['አይነት']}',
-      //     style: kListTextStyle
-      // ),
-      // ),
-      //       trailing: Icon(Icons.arrow_forward_ios),
-      //
-      // );
        return     Card(
                   elevation: 2,
                   child: ListTile(
@@ -136,10 +114,20 @@ import '../constants.dart';
           unselectedItemColor: kInactiveIconColor,
           selectedItemColor: kActiveIconColor,
           showUnselectedLabels: true,
-          items:<BottomNavigationBarItem>[
+            items:<BottomNavigationBarItem>[
                BottomNavigationBarItem(
-                 icon:Icon(Icons.library_music_outlined),
-                 label:'መዝሙሮች'
+
+                 icon:IconButton(
+                   icon: Icon(
+                     Icons.library_music_outlined,
+                       size:_sizeAnimation?.value,
+
+                   ),
+
+                   onPressed: (){},
+                 ),
+                 label:'መዝሙሮች',
+
                ),
             BottomNavigationBarItem(
                 icon:Icon(Icons.music_note_outlined),
@@ -150,6 +138,7 @@ import '../constants.dart';
                 label:'ትምህርቶች'
             ),
             BottomNavigationBarItem(
+
                 icon:Icon(Icons.settings),
                 label:'ማስተካከያ'
             ),
@@ -157,4 +146,4 @@ import '../constants.dart';
         ),
       );
       }
-      }
+}
